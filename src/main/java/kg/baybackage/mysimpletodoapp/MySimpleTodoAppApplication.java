81ab7077d3adcfe087d8ab369d84ad3fc4f8bbde
NV <keldibekov.user@gmail.com>
2025-01-02 23:23:36 +0600

    Initial commit

---
+++ src/main/java/kg/baybackage/mysimpletodoapp/MySimpleTodoAppApplication.java
@@ -0,0 +1,13 @@
+package kg.baybackage.mysimpletodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySimpleTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/baybackage/mysimpletodoapp/MySimpleTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.baybackage.mysimpletodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySimpleTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
